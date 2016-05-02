0485167f0c5bc4b57b27d10ff2c7874bac05e4e1
briannat <briannat@localhost>
2016-05-02 16:15:57 -0400

    final completed project

--- RealWorldBLC/src/edu/saintjoe/cs/brianc/realworld/PosterCollection.java
+++ RealWorldBLC/src/edu/saintjoe/cs/brianc/realworld/PosterCollection.java
@@ -2,4 +2,11 @@ package edu.saintjoe.cs.brianc.realworld;
 
 public class PosterCollection {
 
+	private String band;
+	private String comicbook;
+	private String random;
 }
+	public PosterCollection(String newBand, String newComicBook, String newRandom) {
+		
+				
+	}
---
+++ RealWorldBLC/src/edu/saintjoe/cs/brianc/realworld/PosterCollectionDriver.java
@@ -0,0 +1,8 @@
+package edu.saintjoe.cs.brianc.realworld;
+
+public class PosterCollectionDriver {
+
+	public static void main(String[] args) {
+		
+	}
+}
